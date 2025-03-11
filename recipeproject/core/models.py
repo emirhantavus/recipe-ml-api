94c8f8507f22b53b56708db3832fe444120a1b11
Emirhan Tavus <emirhantavus17@gmail.com>
2025-03-11 06:14:09 +0300

    Add core models and admin registration for recipe management

--- recipeproject/core/models.py
+++ recipeproject/core/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+      title = models.CharField(max_length=255, unique=True) 
+      description = models.TextField(blank=True, null=True)
+      instructions = models.TextField()  # adım adım yapılış için
+      prep_time = models.IntegerField(help_text="Preparation time (minutes)")
+      cook_time = models.IntegerField(help_text="Cooking time (minutes)")
+      servings = models.IntegerField(default=1)
+      created_at = models.DateTimeField(auto_now_add=True)  
+      updated_at = models.DateTimeField(auto_now=True) 
+    
+      def __str__(self):
+            return self.title
+
+
+class Ingredient(models.Model):
+      name = models.CharField(max_length=100, unique=True) #malzeme adı.
+      unit = models.CharField(max_length=50, choices=[
+          ('g', 'Gram'),
+          ('kg', 'Kilogram'),
+          ('ml', 'Milliliter'),
+          ('l', 'Liter'),
+          ('tbsp', 'Tablespoon'),
+          ('tsp', 'Teaspoon'),
+          ('pcs', 'Piece')
+      ])  #birimler.
+      
+      def __str__(self):
+            return self.name
+
+
+class RecipeIngredient(models.Model):
+      recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="ingredients")  
+      ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="recipes")  
+      amount = models.FloatField()  #kullanılan miktar
+      unit = models.CharField(max_length=50)  #ölçü birimi 
+
+      def __str__(self):
+            return f"{self.amount} {self.unit} {self.ingredient.name} ({self.recipe.title})"
+
+class IngredientAlternative(models.Model):
+      original = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="alternatives")  
+      alternative = models.ForeignKey(Ingredient, on_delete=models.CASCADE)  
+      ratio = models.FloatField(default=1.0, help_text="Alternative ingredient ratio")
+      
+      def __str__(self):
+            return f"{self.alternative.name} can replace {self.original.name}"
--- recipeproject/recipeproject/settings.py
+++ recipeproject/recipeproject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'recipeproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'recipedb',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
