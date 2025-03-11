e67c26840762447fae3641f19870c23fa851f9ff
Emirhan Tavus <emirhantavus17@gmail.com>
2025-03-11 06:33:02 +0300

    add user auth model

--- recipeproject/recipeproject/settings.py
+++ recipeproject/recipeproject/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+AUTH_USER_MODEL = 'users.CustomUser'
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework',
+    'users',
 ]
 
 MIDDLEWARE = [
